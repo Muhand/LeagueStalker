d8f5b3bf5a44772b44300afcc46b4aaf04ae54da
Muhand Jumah <muhandjumah@gmail.com>
2018-06-26 15:45:37 -0400

    Edited veiw

--- src/lolStalker/lolStalker/UI/Abstract views/PlayerView.swift
+++ src/lolStalker/lolStalker/UI/Abstract views/PlayerView.swift
@@ -1,3 +1,290 @@
+////
+////  PlayerView.swift
+////  lolStalker
+////
+////  Created by Muhand Jumah on 6/7/18.
+////  Copyright © 2018 Muhand Jumah. All rights reserved.
+////
+//
+//import UIKit
+//
+//protocol PlayerViewDelegate {
+//    func didSetPlayer(player: Player, form: PlayerView)
+//    func cancelPressed(form: PlayerView)
+//}
+//
+//class PlayerView: UIView {
+//    ////////////////////////////////////
+//    //            Delegates
+//    ////////////////////////////////////
+//    var delegate: PlayerViewDelegate?
+//
+//    ////////////////////////////////////
+//    //Global Variables
+//    ////////////////////////////////////
+//    var player: Player? {
+//        didSet{
+//
+//        }
+//    }
+//
+//    ////////////////////////////////////
+//    //            Controls
+//    ////////////////////////////////////
+//    let cancelBtn: UIButton = {
+//        let button = UIButton()
+//        button.setBackgroundColor(color: UIColor(hex: "#010A13")!, forState: .normal)
+//        button.setBackgroundColor(color: UIColor(hex: "#CE9340")!, forState: .highlighted)
+//        button.setTitleColor(UIColor(hex: "#CE9340"), for: .normal)
+//        button.setTitleColor(UIColor(hex: "#010A13"), for: .highlighted)
+//        button.setTitle("CANCEL", for: .normal)
+//        button.layer.borderWidth = Helper.getWidthOf(xdWidth: 1, referenceWidth: nil, originalWidth: nil)
+//        button.layer.borderColor = UIColor(hex: "#CE9340")?.cgColor
+//        button.titleLabel?.font = UIFont.systemFont(ofSize: Helper.getFontOf(xdFont: 12, referenceHeight: nil, referenceWidth: nil))
+//        button.addTarget(self, action: #selector(cancelPressed), for: .touchUpInside)
+//        return button
+//    }()
+//
+//    let rankImage: UIImageView = {
+//        let image = UIImageView()
+//        image.image = UIImage(named: "tempRank")
+//        image.contentMode = .scaleAspectFit
+//        image.layer.masksToBounds = true
+//        image.frame.size = CGSize(width: Helper.getWidthOf(xdWidth: 200, referenceWidth: nil, originalWidth: nil), height: Helper.getWidthOf(xdWidth: 200, referenceWidth: nil, originalWidth: nil))
+//        return image
+//    }()
+//
+//    let rankTitle: UILabel = {
+//        let label = UILabel()
+//        label.text = "Master"
+//        label.textColor = UIColor(hex: "#95989A")
+//        if (UIDevice.current.userInterfaceIdiom == .pad) {
+//            label.font = UIFont.systemFont(ofSize: Helper.getFontOf(xdFont: 40, referenceHeight: nil, referenceWidth: nil))
+//        } else {
+//            label.font = UIFont.systemFont(ofSize: Helper.getFontOf(xdFont: 20, referenceHeight: nil, referenceWidth: nil))
+//        }
+//        label.text = label.text?.capitalized
+//        return label
+//    }()
+//
+//    let minimalStatsView: UIView = {
+//        let view = UIView()
+////        view.backgroundColor = .red
+//        return view
+//    }()
+//
+//    let winRateProgressBar: UIView = {
+//        let view = UIView()
+//        view.backgroundColor = .green
+//        return view
+//    }()
+//
+//    let roleProgressBar: UIView = {
+//        let view = UIView()
+//        view.backgroundColor = .blue
+//        return view
+//    }()
+//
+//    let winRateLabel: UILabel = {
+//        let label = UILabel()
+//        label.text = "Win Rate"
+//        label.textColor = UIColor(hex: "#95989A")
+//        if (UIDevice.current.userInterfaceIdiom == .pad) {
+//            label.font = UIFont.systemFont(ofSize: Helper.getFontOf(xdFont: 40, referenceHeight: nil, referenceWidth: nil))
+//        } else {
+//            label.font = UIFont.systemFont(ofSize: Helper.getFontOf(xdFont: 20, referenceHeight: nil, referenceWidth: nil))
+//        }
+//        label.text = label.text?.capitalized
+//        return label
+//    }()
+//
+//    let roleLabel: UILabel = {
+//        let label = UILabel()
+//        label.text = "Support"
+//        label.textColor = UIColor(hex: "#95989A")
+//        if (UIDevice.current.userInterfaceIdiom == .pad) {
+//            label.font = UIFont.systemFont(ofSize: Helper.getFontOf(xdFont: 40, referenceHeight: nil, referenceWidth: nil))
+//        } else {
+//            label.font = UIFont.systemFont(ofSize: Helper.getFontOf(xdFont: 20, referenceHeight: nil, referenceWidth: nil))
+//        }
+//        label.text = label.text?.capitalized
+//        return label
+//    }()
+//
+//    let winLoseSeparator: UILabel = {
+//        let label = UILabel()
+//        label.text = "-"
+//        label.textColor = UIColor(hex: "#95989A")
+//        if (UIDevice.current.userInterfaceIdiom == .pad) {
+//            label.font = UIFont.systemFont(ofSize: Helper.getFontOf(xdFont: 60, referenceHeight: nil, referenceWidth: nil))
+//        } else {
+//            label.font = UIFont.systemFont(ofSize: Helper.getFontOf(xdFont: 40, referenceHeight: nil, referenceWidth: nil))
+//        }
+//        label.text = label.text?.capitalized
+//        return label
+//    }()
+//
+//    let wins: UILabel = {
+//        let label = UILabel()
+//        label.text = "500W"
+//        label.textColor = UIColor(hex: "#00FF00")
+//        if (UIDevice.current.userInterfaceIdiom == .pad) {
+//            label.font = UIFont.systemFont(ofSize: Helper.getFontOf(xdFont: 60, referenceHeight: nil, referenceWidth: nil))
+//        } else {
+//            label.font = UIFont.systemFont(ofSize: Helper.getFontOf(xdFont: 40, referenceHeight: nil, referenceWidth: nil))
+//        }
+//        label.text = label.text?.capitalized
+//        return label
+//    }()
+//
+//    let loses: UILabel = {
+//        let label = UILabel()
+//        label.text = "249L"
+//        label.textColor = UIColor(hex: "#FF0000")
+//        if (UIDevice.current.userInterfaceIdiom == .pad) {
+//            label.font = UIFont.systemFont(ofSize: Helper.getFontOf(xdFont: 60, referenceHeight: nil, referenceWidth: nil))
+//        } else {
+//            label.font = UIFont.systemFont(ofSize: Helper.getFontOf(xdFont: 40, referenceHeight: nil, referenceWidth: nil))
+//        }
+//        label.text = label.text?.capitalized
+//        return label
+//    }()
+//
+//    let runesSection: PlayerSection = {
+//        let section = PlayerSection()
+//        section.title = "Current Runes"
+//        return section
+//    }()
+//
+//    let testView: UIView = {
+//        let view = UIView()
+//        view.backgroundColor = .purple
+//        let label: UILabel = {
+//            let lab = UILabel()
+//            lab.text = "TEST"
+//            return lab
+//        }()
+//        view.addSubview(label)
+//        label.translatesAutoresizingMaskIntoConstraints = false
+//        label.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+//        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//        return view
+//    }()
+//
+//    ////////// END OF CONTROLS /////////
+//
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        setupView()
+//        setupConstraints()
+//    }
+//
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//
+//    func setupView() {
+//        backgroundColor = UIColor(hex: "#010A13")
+//    }
+//
+//    func setupConstraints() {
+//        // MARK: Add subviews
+//        addSubview(cancelBtn)
+//        addSubview(rankImage)
+//        addSubview(rankTitle)
+//        addSubview(minimalStatsView)
+//        minimalStatsView.addSubview(winRateProgressBar)
+//        minimalStatsView.addSubview(roleProgressBar)
+//        addSubview(winRateLabel)
+//        addSubview(roleLabel)
+//        addSubview(winLoseSeparator)
+//        addSubview(wins)
+//        addSubview(loses)
+//        addSubview(runesSection)
+//        runesSection.content = testView
+//
+//        // MARK: Disable frames
+//        cancelBtn.translatesAutoresizingMaskIntoConstraints = false
+//        rankImage.translatesAutoresizingMaskIntoConstraints = false
+//        rankTitle.translatesAutoresizingMaskIntoConstraints = false
+//        minimalStatsView.translatesAutoresizingMaskIntoConstraints = false
+//        winRateProgressBar.translatesAutoresizingMaskIntoConstraints = false
+//        roleProgressBar.translatesAutoresizingMaskIntoConstraints = false
+//        winRateLabel.translatesAutoresizingMaskIntoConstraints = false
+//        roleLabel.translatesAutoresizingMaskIntoConstraints = false
+//        winLoseSeparator.translatesAutoresizingMaskIntoConstraints = false
+//        wins.translatesAutoresizingMaskIntoConstraints = false
+//        loses.translatesAutoresizingMaskIntoConstraints = false
+//        runesSection.translatesAutoresizingMaskIntoConstraints = false
+//
+//        //////////////////////////////////
+//        // MARK: Constraints
+//        //////////////////////////////////
+//        cancelBtn.widthAnchor.constraint(equalToConstant: Helper.getWidthOf(xdWidth: 76, referenceWidth: nil, originalWidth: nil)).isActive = true
+//        cancelBtn.heightAnchor.constraint(equalToConstant: Helper.getHeightOf(xdHeight: 21.36, referenceHeight: nil, originalHeight: nil)).isActive = true
+//        cancelBtn.topAnchor.constraint(equalTo: topAnchor, constant: Helper.getHeightOf(xdHeight: 20, referenceHeight: nil, originalHeight: nil)).isActive = true
+//        cancelBtn.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+//
+//        // RANK IMAGE
+//        rankImage.widthAnchor.constraint(equalToConstant: Helper.getWidthOf(xdWidth: 200, referenceWidth: nil, originalWidth: nil)).isActive = true
+//        rankImage.heightAnchor.constraint(equalToConstant: Helper.getWidthOf(xdWidth: 200, referenceWidth: nil, originalWidth: nil)).isActive = true
+//        rankImage.topAnchor.constraint(equalTo: topAnchor, constant: 33).isActive = true
+//        rankImage.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+//
+//        // RANK TITLE
+//        rankTitle.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+//        rankTitle.topAnchor.constraint(equalTo: rankImage.bottomAnchor).isActive = true
+//
+//        // MINIMAL STATS (WIN RATE - MOST PLAYED ROLE
+//        minimalStatsView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+//        minimalStatsView.topAnchor.constraint(equalTo: rankTitle.bottomAnchor, constant:20).isActive = true
+//        minimalStatsView.widthAnchor.constraint(equalToConstant: (Helper.getHeightOf(xdHeight: 117, referenceHeight: nil, originalHeight: nil)) * 2 + Helper.getWidthOf(xdWidth: 40, referenceWidth: nil, originalWidth: nil)).isActive = true
+//        minimalStatsView.heightAnchor.constraint(equalToConstant: Helper.getHeightOf(xdHeight: 117, referenceHeight: nil, originalHeight: nil)).isActive = true
+//
+//        // WIN RATE PROGRESS BAR
+//        winRateProgressBar.leftAnchor.constraint(equalTo: minimalStatsView.leftAnchor).isActive = true
+//        winRateProgressBar.topAnchor.constraint(equalTo: minimalStatsView.topAnchor).isActive = true
+//        winRateProgressBar.widthAnchor.constraint(equalTo: minimalStatsView.heightAnchor).isActive = true
+//        winRateProgressBar.heightAnchor.constraint(equalTo: minimalStatsView.heightAnchor).isActive = true
+//
+//        // ROLE PROGRESS BAR
+//        roleProgressBar.rightAnchor.constraint(equalTo: minimalStatsView.rightAnchor).isActive = true
+//        roleProgressBar.topAnchor.constraint(equalTo: minimalStatsView.topAnchor).isActive = true
+//        roleProgressBar.widthAnchor.constraint(equalTo: minimalStatsView.heightAnchor).isActive = true
+//        roleProgressBar.heightAnchor.constraint(equalTo: minimalStatsView.heightAnchor).isActive = true
+//
+//        // Win rate label
+//        winRateLabel.topAnchor.constraint(equalTo: winRateProgressBar.bottomAnchor, constant:5).isActive = true
+//        winRateLabel.centerXAnchor.constraint(equalTo: winRateProgressBar.centerXAnchor).isActive = true
+//
+//        // Role label
+//        roleLabel.topAnchor.constraint(equalTo: roleProgressBar.bottomAnchor, constant: 5).isActive = true
+//        roleLabel.centerXAnchor.constraint(equalTo: roleProgressBar.centerXAnchor).isActive = true
+//
+//        // Wins/Lose
+//        winLoseSeparator.topAnchor.constraint(equalTo: winRateLabel.bottomAnchor, constant: 28).isActive = true
+//        winLoseSeparator.centerXAnchor.constraint(equalTo: minimalStatsView.centerXAnchor).isActive = true
+//
+//        wins.topAnchor.constraint(equalTo: winLoseSeparator.topAnchor).isActive = true
+//        wins.rightAnchor.constraint(equalTo: winLoseSeparator.leftAnchor, constant: -10).isActive = true
+//
+//        loses.topAnchor.constraint(equalTo: winLoseSeparator.topAnchor).isActive = true
+//        loses.leftAnchor.constraint(equalTo: winLoseSeparator.rightAnchor, constant: 10).isActive = true
+//
+//        runesSection.topAnchor.constraint(equalTo: loses.bottomAnchor, constant: 35).isActive = true
+//        runesSection.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
+////        runesSection.heightAnchor.constraint(equalToConstant: 800).isActive = true
+//        runesSection.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+//    }
+//
+//    @IBAction func cancelPressed(sender: AnyObject) {
+//        delegate?.cancelPressed(form: self)
+//    }
+//}
+
+
+
+
 //
 //  PlayerView.swift
 //  lolStalker
@@ -69,7 +356,7 @@ class PlayerView: UIView {
     
     let minimalStatsView: UIView = {
         let view = UIView()
-//        view.backgroundColor = .red
+        //        view.backgroundColor = .red
         return view
     }()
     
@@ -273,7 +560,7 @@ class PlayerView: UIView {
         
         runesSection.topAnchor.constraint(equalTo: loses.bottomAnchor, constant: 35).isActive = true
         runesSection.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
-//        runesSection.heightAnchor.constraint(equalToConstant: 800).isActive = true
+        //        runesSection.heightAnchor.constraint(equalToConstant: 800).isActive = true
         runesSection.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
     }
     
--- src/lolStalker/lolStalker/UI/Player/PlayerViewController.swift
+++ src/lolStalker/lolStalker/UI/Player/PlayerViewController.swift
@@ -1,10 +1,109 @@
+////
+////  PlayerViewController.swift
+////  lolStalker
+////
+////  Created by Muhand Jumah on 6/8/18.
+////  Copyright © 2018 Muhand Jumah. All rights reserved.
+////
 //
-//  PlayerViewController.swift
-//  lolStalker
+//import Foundation
+//import UIKit
 //
-//  Created by Muhand Jumah on 6/8/18.
-//  Copyright © 2018 Muhand Jumah. All rights reserved.
+//class PlayerViewController: UIViewController {
+//    ////////////////////////////////////
+//    //            Controls
+//    ////////////////////////////////////
+//    let parentScrollView: UIScrollView = {
+//        let view = UIScrollView()
+//        view.backgroundColor = .red
+//        return view
+//    }()
 //
+//    let playerView: PlayerView = {
+//        let view = PlayerView()
+//
+////        var player: Player = Player()
+////        player.summonerName = "WarDesigner"
+////        player.championName = "Aatrox"
+////        player.backgroundImage = #imageLiteral(resourceName: "tempChampion")
+////        player.profileImage = #imageLiteral(resourceName: "tempProfile")
+////        player.spell1 = #imageLiteral(resourceName: "tempSpell1")
+////        player.spell2 = #imageLiteral(resourceName: "tempSpell2")
+//
+////        var team: Team
+//
+////        do {
+////            team = try Team(players: player, player, player, player, player)
+////            view.game = Game(team1: team, team2: team)
+////        } catch {
+////            print(error)
+////        }
+//
+//
+//        return view
+//    }()
+//
+//    ////////// END OF CONTROLS /////////
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        setupView()
+//        setupSubViews()
+//        setupConstraints()
+//        setupMargins()
+//    }
+//
+//    func setupView() {
+//        view.backgroundColor = UIColor(hex: "#010A13")
+////        view.backgroundColor = .green
+//        hideKeyboardWhenTappedAround()
+//    }
+//
+//    func setupSubViews() {
+//        view.addSubview(playerView)
+////        parentScrollView.addSubview(playerView)
+//
+//        playerView.delegate = self
+//    }
+//
+//    func setupConstraints() {
+//        parentScrollView.translatesAutoresizingMaskIntoConstraints = false
+//        playerView.translatesAutoresizingMaskIntoConstraints = false
+//
+////        view.addConstraintsWithFormat(format: "V:|[v0]|", views: parentScrollView)
+////        view.addConstraintsWithFormat(format: "H:|[v0]|", views: parentScrollView)
+//
+////        parentScrollView.addConstraintsWithFormat(format: "H:|-\(Helper.getWidthOf(xdWidth: 12, referenceWidth: nil, originalWidth: nil))-[v0]-\(Helper.getWidthOf(xdWidth: 12, referenceWidth: nil, originalWidth: nil))-|", views: playerView)
+////        parentScrollView.addConstraintsWithFormat(format: "H:|[v0]|", views: playerView)
+////        parentScrollView.addConstraintsWithFormat(format: "V:|[v0]|", views: playerView)
+//
+////        parentScrollView.addConstraintsWithFormat(format: "V:|-\(UIApplication.shared.statusBarFrame.height)-[v0]|", views: playerView)
+//
+//        playerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//        playerView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+//        playerView.heightAnchor.constraint(equalTo: view.heightAnchor, constant: -UIApplication.shared.statusBarFrame.height).isActive = true
+//        playerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -Helper.getWidthOf(xdWidth: 24, referenceWidth: nil, originalWidth: nil)).isActive = true
+//
+//    }
+//
+//    func setupMargins() {
+//    }
+//
+//    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+//        super.viewWillTransition(to: size, with: coordinator)
+//        view.setNeedsDisplay()
+//    }
+//
+//}
+//
+//extension PlayerViewController: PlayerViewDelegate {
+//    func cancelPressed(form: PlayerView) {
+//        dismiss(animated: true, completion: nil)
+//    }
+//
+//    func didSetPlayer(player: Player, form: PlayerView) {
+//
+//    }
+//}
 
 import Foundation
 import UIKit
@@ -13,79 +112,91 @@ class PlayerViewController: UIViewController {
     ////////////////////////////////////
     //            Controls
     ////////////////////////////////////
-    let parentScrollView: UIScrollView = {
-        let view = UIScrollView()
-        view.backgroundColor = .red
+//    let parentView: UIView = {
+//        let view = UIView()
+//        return view
+//    }()
+    
+    let cancelBtn: UIButton = {
+        let button = UIButton()
+        button.setBackgroundColor(color: UIColor(hex: "#010A13")!, forState: .normal)
+        button.setBackgroundColor(color: UIColor(hex: "#CE9340")!, forState: .highlighted)
+        button.setTitleColor(UIColor(hex: "#CE9340"), for: .normal)
+        button.setTitleColor(UIColor(hex: "#010A13"), for: .highlighted)
+        button.setTitle("CANCEL", for: .normal)
+        button.layer.borderWidth = Helper.getWidthOf(xdWidth: 1, referenceWidth: nil, originalWidth: nil)
+        button.layer.borderColor = UIColor(hex: "#CE9340")?.cgColor
+        button.titleLabel?.font = UIFont.systemFont(ofSize: Helper.getFontOf(xdFont: 12, referenceHeight: nil, referenceWidth: nil))
+        button.addTarget(self, action: #selector(cancelPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    let topContentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .green
         return view
     }()
     
-    let playerView: PlayerView = {
-        let view = PlayerView()
-        
-//        var player: Player = Player()
-//        player.summonerName = "WarDesigner"
-//        player.championName = "Aatrox"
-//        player.backgroundImage = #imageLiteral(resourceName: "tempChampion")
-//        player.profileImage = #imageLiteral(resourceName: "tempProfile")
-//        player.spell1 = #imageLiteral(resourceName: "tempSpell1")
-//        player.spell2 = #imageLiteral(resourceName: "tempSpell2")
-        
-//        var team: Team
-        
-//        do {
-//            team = try Team(players: player, player, player, player, player)
-//            view.game = Game(team1: team, team2: team)
-//        } catch {
-//            print(error)
-//        }
-        
-        
-        return view
-    }()
+//    let restOfContentView: UICollectionView = {
+//        let view = UICollectionView()
+//        view.backgroundColor = .purple
+//        return view
+//    }()
     
     ////////// END OF CONTROLS /////////
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
         setupSubViews()
         setupConstraints()
-        setupMargins()
     }
     
     func setupView() {
-        view.backgroundColor = UIColor(hex: "#010A13")
-//        view.backgroundColor = .green
+//        view.backgroundColor = UIColor(hex: "#010A13")
+        view.backgroundColor = .red
         hideKeyboardWhenTappedAround()
+//        restOfContentView.delegate = self
     }
     
     func setupSubViews() {
-        view.addSubview(playerView)
-//        parentScrollView.addSubview(playerView)
-        
-        playerView.delegate = self
+//        view.addSubview(parentView)
+        view.addSubview(cancelBtn)
+        view.addSubview(topContentView)
+//        parentView.addSubview(restOfContentView)
     }
     
     func setupConstraints() {
-        parentScrollView.translatesAutoresizingMaskIntoConstraints = false
-        playerView.translatesAutoresizingMaskIntoConstraints = false
+        // Disable frames
+//        parentView.translatesAutoresizingMaskIntoConstraints = false
+        cancelBtn.translatesAutoresizingMaskIntoConstraints = false
+        topContentView.translatesAutoresizingMaskIntoConstraints = false
+//        restOfContentView.translatesAutoresizingMaskIntoConstraints = false
         
-//        view.addConstraintsWithFormat(format: "V:|[v0]|", views: parentScrollView)
-//        view.addConstraintsWithFormat(format: "H:|[v0]|", views: parentScrollView)
+        // Setup constraints
+//        parentView.topAnchor.constraint(equalTo: view.topAnchor, constant: UIApplication.shared
+//            .statusBarFrame.height).isActive = true
+//        parentView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+//        parentView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: Helper.getWidthOf(xdWidth: 12, referenceWidth: nil, originalWidth: nil)).isActive = true
+//        parentView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -Helper.getWidthOf(xdWidth: 12, referenceWidth: nil, originalWidth: nil)).isActive = true
+//
+        // Cancel Button
+        cancelBtn.widthAnchor.constraint(equalToConstant: Helper.getWidthOf(xdWidth: 76, referenceWidth: nil, originalWidth: nil)).isActive = true
+        cancelBtn.heightAnchor.constraint(equalToConstant: Helper.getHeightOf(xdHeight: 21.36, referenceHeight: nil, originalHeight: nil)).isActive = true
+        cancelBtn.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        cancelBtn.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         
-//        parentScrollView.addConstraintsWithFormat(format: "H:|-\(Helper.getWidthOf(xdWidth: 12, referenceWidth: nil, originalWidth: nil))-[v0]-\(Helper.getWidthOf(xdWidth: 12, referenceWidth: nil, originalWidth: nil))-|", views: playerView)
-//        parentScrollView.addConstraintsWithFormat(format: "H:|[v0]|", views: playerView)
-//        parentScrollView.addConstraintsWithFormat(format: "V:|[v0]|", views: playerView)
+        // Top content view
+        topContentView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        topContentView.heightAnchor.constraint(equalTo: view.heightAnchor, constant: -200).isActive = true
+        topContentView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        topContentView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         
-//        parentScrollView.addConstraintsWithFormat(format: "V:|-\(UIApplication.shared.statusBarFrame.height)-[v0]|", views: playerView)
-        
-        playerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        playerView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-        playerView.heightAnchor.constraint(equalTo: view.heightAnchor, constant: -UIApplication.shared.statusBarFrame.height).isActive = true
-        playerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -Helper.getWidthOf(xdWidth: 24, referenceWidth: nil, originalWidth: nil)).isActive = true
-        
-    }
-    
-    func setupMargins() {
+        // Rest of the content
+//        restOfContentView.widthAnchor.constraint(equalTo: parentView.widthAnchor).isActive = true
+//        restOfContentView.topAnchor.constraint(equalTo: topContentView.bottomAnchor).isActive = true
+//        restOfContentView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+//        restOfContentView.centerXAnchor.constraint(equalTo: parentView.centerXAnchor).isActive = true
     }
     
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
@@ -93,16 +204,19 @@ class PlayerViewController: UIViewController {
         view.setNeedsDisplay()
     }
     
-}
-
-extension PlayerViewController: PlayerViewDelegate {
-    func cancelPressed(form: PlayerView) {
+    override func viewWillLayoutSubviews() {
+        view.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width - 24, height: 400)
+    }
+    
+    ////////////////////////////////////
+    //            Actions
+    ////////////////////////////////////
+    @IBAction func cancelPressed(sender: AnyObject) {
         dismiss(animated: true, completion: nil)
     }
-    
-    func didSetPlayer(player: Player, form: PlayerView) {
-        
-    }
-    
+}
+
+
+extension PlayerViewController: UICollectionViewDelegate {
     
 }
